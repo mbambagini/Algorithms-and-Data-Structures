a2d3b2578b672fd11d7a30fab5b295bcefa87a89
Mario <mario.bambagini@gmail.com>
2015-09-11 10:35:46 +0200

    Update list.cc
    added push and pop interface

--- list.cc
+++ list.cc
@@ -1,8 +1,9 @@
 //This file contains the declaration and implementation of a generic list
-//Lists work better than array to handle data whose number varies 
-//significantly (at any time the allocated memory is equal to the amount
-//of requried data and the maximum number of elements is not strictly
-//bounded)
+//Lists work better than array to handle data whose number varies significantly
+//(at any time the allocated memory is equal to the amount of required data and
+//the maximum number of elements is not strictly bounded)
+
+//many functions may seem useless, but their duty consists of making practise
 
 #include <iostream>
 #include <assert.h>
@@ -11,9 +12,31 @@
 using namespace std;
 
 //This class implements a simple single-linked list
+//The implementation implicitly assumes that there is a constructor of T which
+//accepts an int as input (for handling errors: to avoid exceptions)
 template <class T>
 class MyList {
  public:
+
+  //insert a new element at the list start
+  //complexity O(1);
+  void pushFront (T value) {
+    insert(value);
+  }
+  
+  //insert a new element at the list end
+  //complexity O(n)
+  void pushBack (T value);
+  
+  //delete and return the first element of the list (note that the functions
+  //top and pop should have been implemented to avoid data inconsistency -
+  //however it is out of the scope of this code)
+  //complexity O(1)
+  T popFront ();
+  
+  //delete and return the last element of the list
+  //complexity O(n)
+  T popBack ();
 
   //insert value as first element in the list
   //complexity O(1)
@@ -80,7 +103,7 @@ T MyList<T>::at (int i) {
   }
 
   if (ptr == NULL)
-    return 0;
+    return -1;
 
   return ptr->val;
 }
@@ -98,9 +121,11 @@ void MyList<T>::insert (T value) {
 
 template<class T>
 bool MyList<T>::insertAfter(T value, T after) {
+  //empty list
   if (head == NULL)
     return false;
 
+  //generic case
   Node tmp = head;
   while (tmp != NULL) {
     if (tmp->value == after && (tmp->next == NULL || tmp->value != after) ) {
@@ -119,9 +144,11 @@ bool MyList<T>::insertAfter(T value, T after) {
 
 template<class T>
 bool MyList<T>::remove(T value) {
+  //empty list
   if (head == NULL)
     return false;
 
+  //first element is the wanted
   if (head->val == value) {
     Node *n = head;
 	head = head->next;
@@ -130,6 +157,7 @@ bool MyList<T>::remove(T value) {
 	return true;
   }
 
+  //generic case
   Node *tmp = head;
   while (tmp->next != NULL) {
     if (tmp->next->val == value) {
@@ -203,6 +231,73 @@ bool MyList<T>::anyDuplicate () {
   return false;
 }
 
+template<class T>
+void MyList<T>::pushBack (T value) {
+  Node *n = new Node;
+  n->val = value;
+  n->next = NULL;
+
+  //empty list
+  if (head == NULL) {
+    head = n;
+	num_nodes++;
+	return;
+  }
+
+  //generic case
+  Node *ptr = head;
+  while (ptr->next != NULL)
+    ptr = ptr->next;
+
+  ptr->next = n;
+  num_nodes++;
+}
+
+template<class T>
+T MyList<T>::popFront () {
+  //empty list
+  if (head == NULL)
+    return -1;
+
+  //generic case
+  T ret = head->val;
+  Node *n = head;
+  head = head->next;
+  num_nodes--;
+  delete n;
+
+  return ret;
+}
+
+template<class T>
+T MyList<T>::popBack () {
+  //empty list
+  if (head == NULL)
+    return -1;
+
+  //only one element
+  if (head->next == NULL) {
+    T t = head->val;
+	Node *n = head;
+	head = NULL;
+	delete n;
+	num_nodes--;
+	return t;
+  }
+
+  //generic case
+  Node *ptr = head;
+  while (ptr->next->next != NULL)
+    ptr = ptr->next;
+
+  T t = ptr->next->val;
+  Node *n = ptr->next;
+  ptr->next = ptr->next->next;
+  delete n;
+  num_nodes--;
+  return t;
+}
+
 int main () {
   MyList<int> mylist;
   
@@ -237,5 +332,20 @@ int main () {
   assert(mylist.unique(20)==false);
   assert(mylist.anyDuplicate()==true);
 
+  //push and pop
+  MyList<int> mylist2;
+  mylist2.insert(20);
+  mylist2.pushBack(30);
+  mylist2.pushFront(10);
+  assert(mylist2.at(0)==10);
+  assert(mylist2.at(1)==20);
+  assert(mylist2.at(2)==30);
+  assert(mylist2.size()==3);
+  assert(mylist2.popBack()==30);
+  assert(mylist2.popFront()==10);
+  assert(mylist2.size()==1);
+  assert(mylist2.popBack()==20);
+  assert(mylist2.size()==0);
+  
   return 0;
 }
