3a11a4b53e618133fdaac7737cf48dd42c6a9046
Mario <mario.bambagini@gmail.com>
2015-09-11 12:36:29 +0200

    Update run.sh
    output management updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 files=( "list" "double_list" )
 
@@ -6,8 +6,8 @@ for file in "${files[@]}"
 do
   echo -ne "FILE $file: "
   g++ $file.cc -o $file.o
-  ./$file.o 2> output.tmp
-  if [[ -s $file ]] ; then
+  { ./$file.o; } 2> output.tmp
+  if [[ -s output.tmp ]] ; then
     echo "ERRORS"
   else
     echo "SUCCESS!"
