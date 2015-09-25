781087517c152f087a7ddf76e179964c8027925b
Mario <mario.bambagini@gmail.com>
2015-09-25 10:33:18 +0200

    Update run.sh
    added Dijkstra's algorithm

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 files=( "list" "double_list" "stack" "queue" "deque" "heap" "tree" "trie"
         "hash" "graph_matrix" "graph_list"
-        "sorting" "search"
+        "sorting" "search" "dijkstra"
         "examples/ex_17_1" "examples/ex_17_2" "examples/ex_17_3"
         "examples/ex_17_4" "examples/ex_17_5" "examples/ex_17_6"
         "examples/ex_17_7" "examples/ex_17_9" "examples/ex_17_11"
